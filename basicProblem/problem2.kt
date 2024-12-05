be1143c53120c214fe8c19e7d5df1a239ba778db
aryan <guptaaryan91236@gmail.com>
2024-12-05 21:39:38 +0530

    solved

---
+++ basicProblem/problem2.kt
@@ -0,0 +1,6 @@
+//Write a Kotlin program to find out what version of Kotlin you are using.
+fun main() {
+    val kotlinVersion = KotlinVersion.CURRENT
+    println("Current Kotlin version: $kotlinVersion")
+}
+
