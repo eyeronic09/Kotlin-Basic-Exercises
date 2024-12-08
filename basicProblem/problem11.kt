ea4b822b54a45d4e15b1c4df4e35a38f1ab6e4b1
aryan <guptaaryan91236@gmail.com>
2024-12-08 17:57:54 +0530

    solved basic all problem

---
+++ basicProblem/problem11.kt
@@ -0,0 +1,31 @@
+fun celsiusToFahrenheit(c: Double) : Double{
+    return (c  * 9 / 5) + 32
+}
+fun fahrenheitToCelsius(f : Double): Double {
+    return (f - 32) * 5 / 9
+}
+fun main() {
+    println("choice an option")
+    println("1. Convert Celsius to Fahrenheit")
+    println("2. Convert Fahrenheit to Celsius")
+    val option = readln().toIntOrNull()
+
+    if (option != null && (option == 1 || option == 2 )) {
+        println("please enter the temperature ")
+        val temperature = readln().toDouble()
+        when(option){
+            1 -> {
+                val fahrenheit = celsiusToFahrenheit(temperature)
+                println("Temperature in Celsius: $temperature")
+                println("Temperature in Fahrenheit: $fahrenheit")
+            }
+            2 ->{
+                val celsius  = fahrenheitToCelsius(temperature)
+                println("Temperature in Celsius: $temperature")
+                println("Temperature in Fahrenheit: $celsius")
+            }
+    }
+    }else{
+        println("invalid option ")
+    }
+}
